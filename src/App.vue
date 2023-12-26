9015ea55651b37614ac71b0f376225658206dac3
john <aarontu803@gmail.com>
2023-12-26 18:00:26 +0800

    feat:css header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import HeaderComp from "./components/HeaderComp.vue";
 </script>
 
 <template>
+  <HeaderComp></HeaderComp> 
   <RouterView></RouterView>
 </template>
 
