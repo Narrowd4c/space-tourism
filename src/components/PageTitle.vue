b6a98f2729ed6371c110b7de94328e75a6357430
john <aarontu803@gmail.com>
2023-12-28 13:25:18 +0800

    feat: css crewPage

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="flex items-center pb-20 font-barlow text-3xl">
+  <h1 class="flex items-center font-barlow text-3xl">
     <span class="me-4 text-2xl text-gray-500">{{ pageNumber }}</span
     >{{ title }}
   </h1>
